5d9a8884aad8a8cdd814835c64224e5664bad846
manish kumar <manish.nandan@gmail.com>
2013-07-28 20:00:05 +0530

    intial commit

---
+++ web.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var express = require('express');
+var app = express.createServer(express.logger());
+
+var res = fs.readFileSync("index.html");
+
+app.get('/', function(request, response) {
+  response.send(res.toString("utf8"));
+
+//app.get('/', function(request, response) {
+ // response.send('Hello World!');
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
